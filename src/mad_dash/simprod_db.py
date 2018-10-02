092cee35f8ccc937c92b8c88ff258469aa1a59e3
Alex Olivas <olivas@icecube.umd.edu>
2018-10-02 12:34:52 -0600

    hope this works

--- src/mad_dash/simprod_db.py
+++ src/mad_dash/simprod_db.py
@@ -2,6 +2,7 @@ import os
 from os.path import join
 from pymongo import MongoClient
 
-f = open(join(os.environ['HOME'],'.mongo'))
+#f = open(join(os.environ['HOME'],'.mongo'))
+f = open(join(os.environ['MONGO'],'.mongo'))
 url = "mongodb://DBadmin:%s@mongodb-simprod.icecube.wisc.edu" % f.readline().strip()
 spdb = MongoClient(url)
