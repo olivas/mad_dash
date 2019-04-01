056a1535318184dfabb4200d50466e7454430f27
Alex Olivas <olivas@icecube.umd.edu>
2019-04-01 08:54:25 -0600

    remove dead code

--- src/mad_dash/simprod_db.py
+++ src/mad_dash/simprod_db.py
@@ -3,7 +3,6 @@ from os.path import join
 from pymongo import MongoClient
 
 #f = open(join(os.environ['HOME'],'.mongo'))
-#f = open(join(os.environ['MONGO'],'.mongo')
 f = open('/home/olivas/.mongo')
 url = "mongodb://DBadmin:%s@mongodb-simprod.icecube.wisc.edu" % f.readline().strip()
 spdb = MongoClient(url)
