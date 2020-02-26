30f6625a78c922c628dc8d72e1fe8c8c81d34def
Ric Evans <eevans@icecube.wisc.edu>
2020-02-25 18:22:27 -0600

    change dash.Dash name to support local css

--- application.py
+++ application.py
@@ -2,6 +2,6 @@
 import dash
 import flask
 
-app = dash.Dash('Mad Dash', server = flask.Flask(__name__))
+app = dash.Dash(__name__, server = flask.Flask(__name__))
 server = app.server
 app.config.suppress_callback_exceptions = True
