3643e579c53c6eda34411a90052c9e7c9acdd96c
Ric Evans <emejqz@gmail.com>
2020-06-02 16:35:58 -0500

    typing

--- db_server/rest_server.py
+++ db_server/rest_server.py
@@ -2,12 +2,17 @@
 
 import asyncio
 import logging
+from typing import Any, Dict, List, Tuple, Union
 from urllib.parse import quote_plus
 
 import tornado.web
-from motor.motor_tornado import MotorClient
-from rest_tools.client import json_decode
-from rest_tools.server import RestHandler, RestHandlerSetup, RestServer, from_environment, handler
+from motor.motor_tornado import MotorClient, MotorCollection, MotorDatabase  # type: ignore
+from rest_tools.client import json_decode  # type: ignore
+from rest_tools.server import (RestHandler, RestHandlerSetup, RestServer,  # type: ignore
+                               from_environment, handler)
+
+# -----------------------------------------------------------------------------
+
 
 AUTH_PREFIX = "maddash"
 
@@ -28,36 +33,37 @@ EXCLUDE_DBS = ['system.indexes', 'production', 'local',
 
 REMOVE_ID = {"_id": False}
 
+
 # -----------------------------------------------------------------------------
 
 
 class MadDashMotorClient():
     """MotorClient with additional guardrails for Mad-Dash things."""
 
-    def __init__(self, motor_client):
-        """Set-up."""
+    def __init__(self, motor_client: MotorClient) -> None:
+        """Init."""
         self.motor_client = motor_client
 
-    async def get_database_names(self):
+    async def get_database_names(self) -> List[str]:
         """Return all databases' names."""
         database_names = [n for n in await self.motor_client.list_database_names() if n not in EXCLUDE_DBS]
         return database_names
 
-    def get_database(self, database_name):
+    def get_database(self, database_name: str) -> MotorDatabase:
         """Return database instance."""
         try:
             return self.motor_client[database_name]
         except (KeyError, TypeError):
             raise tornado.web.HTTPError(400, reason=f"database not found ({database_name})")
 
-    async def get_collection_names(self, database_name):
+    async def get_collection_names(self, database_name: str) -> List[str]:
         """Return collection names in database."""
         database = self.get_database(database_name)
         collection_names = [n for n in await database.list_collection_names() if n != 'system.indexes']
 
         return collection_names
 
-    def get_collection(self, database_name, collection_name):
+    def get_collection(self, database_name: str, collection_name: str) -> MotorCollection:
         """Return collection instance."""
         database = self.get_database(database_name)
         try:
@@ -65,20 +71,20 @@ class MadDashMotorClient():
         except KeyError:
             raise tornado.web.HTTPError(400, reason=f"collection not found ({collection_name})")
 
-    async def ensure_collection_indexes(self, database_name, collection_name):
+    async def ensure_collection_indexes(self, database_name: str, collection_name: str) -> None:
         """Create indexes in collection."""
         collection = self.get_collection(database_name, collection_name)
         await collection.create_index('name', name='name_index', unique=True)
         async for index in collection.list_indexes():
             print(index)
 
-    async def ensure_all_databases_indexes(self):
+    async def ensure_all_databases_indexes(self) -> None:
         """Create all indexes in all databases."""
         for database_name in await self.get_database_names():
             for collection_name in await self.get_collection_names(database_name):
                 await self.ensure_collection_indexes(database_name, collection_name)
 
-    async def get_create_collection(self, database_name, collection_name):
+    async def get_create_collection(self, database_name: str, collection_name: str) -> MotorCollection:
         """Return collection instance, if it doesn't exist, create it."""
         database = self.get_database(database_name)
         try:
@@ -89,7 +95,7 @@ class MadDashMotorClient():
 
         return collection
 
-    async def get_histograms_in_collection(self, database_name, collection_name):
+    async def get_histograms_in_collection(self, database_name: str, collection_name: str) -> List[dict]:
         """Return collection's contents."""
         collection = self.get_collection(database_name, collection_name)
 
@@ -99,26 +105,27 @@ class MadDashMotorClient():
 
         return [c for c in objs if c['name'] != 'filelist']
 
+
 # -----------------------------------------------------------------------------
 
 
 class BaseMadDashHandler(RestHandler):
     """BaseMadDashHandler is a RestHandler for all Mad-Dash routes."""
 
-    def initialize(self, motor_client, *args, **kwargs):  # pylint: disable=W0221
+    def initialize(self, motor_client: MotorClient, *args, **kwargs) -> None:  # pylint: disable=W0221
         """Initialize a BaseMadDashHandler object."""
         super(BaseMadDashHandler, self).initialize(*args, **kwargs)
         # self.motor_client = motor_client  # pylint: disable=W0201
         self.md_mc = MadDashMotorClient(motor_client)  # pylint: disable=W0201
 
-    def get_optional_argument(self, name, default=None):
+    def get_optional_argument(self, name: str, default: Any = None) -> Any:
         """Return argument, or default value if not present."""
         try:
             return self.get_required_argument(name)
         except tornado.web.HTTPError:
             return default
 
-    def get_required_argument(self, name):
+    def get_required_argument(self, name: str) -> Any:
         """Return argument, raise 400 if not present."""
         if self.request.body:
             try:
@@ -139,9 +146,10 @@ class BaseMadDashHandler(RestHandler):
 class MainHandler(BaseMadDashHandler):
     """MainHandler is a BaseMadDashHandler that handles the root route."""
 
-    def get(self):
+    def get(self) -> None:
         """Handle GET."""
         self.write({})
+
 
 # -----------------------------------------------------------------------------
 
@@ -150,11 +158,12 @@ class DatabasesNamesHandler(BaseMadDashHandler):
     """Handle querying list of databases in mongodb client."""
 
     @handler.scope_role_auth(prefix=AUTH_PREFIX, roles=['production', 'web'])
-    async def get(self):
+    async def get(self) -> None:
         """Handle GET."""
         database_names = await self.md_mc.get_database_names()
 
         self.write({'databases': database_names})
+
 
 # -----------------------------------------------------------------------------
 
@@ -163,7 +172,7 @@ class CollectionsNamesHandler(BaseMadDashHandler):
     """Handle querying list of collections from specified database."""
 
     @handler.scope_role_auth(prefix=AUTH_PREFIX, roles=['production', 'web'])
-    async def get(self):
+    async def get(self) -> None:
         """Handle GET."""
         database_name = self.get_required_argument('database')
 
@@ -172,6 +181,7 @@ class CollectionsNamesHandler(BaseMadDashHandler):
         self.write({'database': database_name,
                     'collections': collection_names})
 
+
 # -----------------------------------------------------------------------------
 
 
@@ -179,7 +189,7 @@ class CollectionsHistogramsNamesHandler(BaseMadDashHandler):
     """Handle querying list of histograms' names."""
 
     @handler.scope_role_auth(prefix=AUTH_PREFIX, roles=['production', 'web'])
-    async def get(self):
+    async def get(self) -> None:
         """Handle GET."""
         database_name = self.get_required_argument('database')
         collection_name = self.get_required_argument('collection')
@@ -198,7 +208,7 @@ class CollectionsHistogramsHandler(BaseMadDashHandler):
     """Handle querying list of histogram objects in given collection."""
 
     @handler.scope_role_auth(prefix=AUTH_PREFIX, roles=['production', 'web'])
-    async def get(self):
+    async def get(self) -> None:
         """Handle GET."""
         database_name = self.get_required_argument('database')
         collection_name = self.get_required_argument('collection')
@@ -209,13 +219,14 @@ class CollectionsHistogramsHandler(BaseMadDashHandler):
                     'collection': collection_name,
                     'histograms': histograms})
 
+
 # -----------------------------------------------------------------------------
 
 
 class HistogramHandler(BaseMadDashHandler):
     """Handle querying/adding histogram object."""
 
-    async def get_histogram(self, database_name, collection_name, histogram_name, remove_id=True):
+    async def get_histogram(self, database_name: str, collection_name: str, histogram_name: str, remove_id: bool=True) -> dict:
         """Return histogram object."""
         collection = self.md_mc.get_collection(database_name, collection_name)
 
@@ -227,7 +238,7 @@ class HistogramHandler(BaseMadDashHandler):
         return histogram
 
     @handler.scope_role_auth(prefix=AUTH_PREFIX, roles=['production', 'web'])
-    async def get(self):
+    async def get(self) -> None:
         """Handle GET."""
         database_name = self.get_required_argument('database')
         collection_name = self.get_required_argument('collection')
@@ -242,7 +253,7 @@ class HistogramHandler(BaseMadDashHandler):
                     'histogram': histogram})
 
     @staticmethod
-    def verify_histogram_schema(histogram):
+    def verify_histogram_schema(histogram: dict) -> None:
         """Raise tornado errors if the histogram already exists or is not well structured."""
         schema = {'name': str,
                   'xmax': (int, float),
@@ -250,7 +261,7 @@ class HistogramHandler(BaseMadDashHandler):
                   'overflow': int,
                   'underflow': int,
                   'nan_count': int,
-                  'bin_values': list}
+                  'bin_values': list}  # type: Dict[str, Union[type, Tuple[type, ...]]]
 
         # check fields
         missing_keys = schema.keys() - set(histogram.keys())
@@ -265,12 +276,12 @@ class HistogramHandler(BaseMadDashHandler):
         if histogram['name'] == 'filelist':
             raise tornado.web.HTTPError(400, reason="histogram cannot be named 'filelist'")
 
-    async def histogram_exists(self, database_name, collection_name, histogram_name):
+    async def histogram_exists(self, database_name: str, collection_name: str, histogram_name: str) -> bool:
         """Return whether histogram already exists."""
         exists = bool(await self.get_histogram(database_name, collection_name, histogram_name))
         return exists
 
-    async def update_histogram(self, database_name, collection_name, histogram):
+    async def update_histogram(self, database_name: str, collection_name: str, histogram: dict) -> bool:
         """Update the histogram's values. Assumes the histogram already exits."""
         prev_histo = await self.get_histogram(database_name, collection_name,
                                               histogram['name'], remove_id=False)
@@ -287,7 +298,7 @@ class HistogramHandler(BaseMadDashHandler):
         return result.acknowledged
 
     @handler.scope_role_auth(prefix=AUTH_PREFIX, roles=['production'])
-    async def post(self):
+    async def post(self) -> None:
         """Handle POST."""
         database_name = self.get_required_argument('database')
         collection_name = self.get_required_argument('collection')
@@ -312,13 +323,14 @@ class HistogramHandler(BaseMadDashHandler):
                     'histogram': {k: histogram[k] for k in histogram if k != '_id'},
                     'updated': histo_updated})
 
+
 # -----------------------------------------------------------------------------
 
 
 class FileNamesHandler(BaseMadDashHandler):
     """Handle querying list of filenames for given collection."""
 
-    async def get_filelist(self, database_name, collection_name, remove_id=True):
+    async def get_filelist(self, database_name: str, collection_name: str, remove_id: bool=True) -> Dict[str, List[str]]:
         """Return list of files in collection."""
         collection = self.md_mc.get_collection(database_name, collection_name)
 
@@ -327,12 +339,12 @@ class FileNamesHandler(BaseMadDashHandler):
         return await collection.find_one({'name': 'filelist'})
 
     @handler.scope_role_auth(prefix=AUTH_PREFIX, roles=['production', 'web'])
-    async def get(self):
+    async def get(self) -> None:
         """Handle GET."""
         database_name = self.get_required_argument('database')
         collection_name = self.get_required_argument('collection')
 
-        filenamelist = []
+        filenamelist = []  # type: List[str]
         filelist = await self.get_filelist(database_name, collection_name)
         if filelist:
             filenamelist = filelist['files']
@@ -341,7 +353,7 @@ class FileNamesHandler(BaseMadDashHandler):
                     'collection': collection_name,
                     'files': filenamelist})
 
-    async def update_filelist(self, database_name, collection_name, filenamelist):
+    async def update_filelist(self, database_name: str, collection_name: str, filenamelist: List[str]) -> Tuple[bool, List[str]]:
         """Update (extends) filelist. Assumes the filelist already exits."""
         prev_filelist = await self.get_filelist(database_name, collection_name, remove_id=False)
 
@@ -355,7 +367,7 @@ class FileNamesHandler(BaseMadDashHandler):
         return result.acknowledged, filenamelist
 
     @handler.scope_role_auth(prefix=AUTH_PREFIX, roles=['production'])
-    async def post(self):
+    async def post(self) -> None:
         """Handle POST."""
         database_name = self.get_required_argument('database')
         collection_name = self.get_required_argument('collection')
@@ -386,7 +398,7 @@ class FileNamesHandler(BaseMadDashHandler):
 # -----------------------------------------------------------------------------
 
 
-def start(debug=False):
+def start(debug: bool = False) -> RestServer:
     """Start a Mad Dash REST service."""
     config = from_environment(EXPECTED_CONFIG)
 
@@ -438,7 +450,7 @@ def start(debug=False):
     return server
 
 
-def main():
+def main() -> None:
     """Configure logging and start a Mad Dash DB service."""
     logging.basicConfig(level=logging.DEBUG)
     start(debug=True)
