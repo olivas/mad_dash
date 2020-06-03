56a209a72208b534d7548ce2889c1f792273c194
Ric Evans <emejqz@gmail.com>
2020-06-03 12:54:52 -0500

    comments & cleanup

--- web_app/web_app/tabs/comparison_tab.py
+++ web_app/web_app/tabs/comparison_tab.py
@@ -1,54 +1,17 @@
-# -*- coding: utf-8 -*-
-from copy import copy
-# from statistics.compare import compare
+"""Dash tab for comparing histograms."""
+
 from typing import Dict, List
 
 import dash_core_components as dcc  # type: ignore
 import dash_daq as daq  # type: ignore
 import dash_html_components as html  # type: ignore
 import plotly.graph_objs as go  # type: ignore
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output  # type: ignore
 
 from ..config import app
-from ..styles import (CENTERED_30, CENTERED_100, SHORT_HR, STAT_LABEL, STAT_NUMBER, WIDTH_30,
-                      WIDTH_45)
+from ..styles import CENTERED_100, SHORT_HR, WIDTH_45
 from ..utils import db, histogram_converter
 from .database_controls import get_database_name_options, get_default_database
-
-
-def cdf(histogram):
-    cdf_histogram = copy(histogram)
-    v = 0
-    for idx, bv in enumerate(histogram['bin_values']):
-        v += bv
-        histogram['bin_values'][idx] = v
-    return histogram
-
-
-def scale_to_match(h1, h2):
-
-    n1 = sum(h1['bin_values'])
-    n2 = sum(h2['bin_values'])
-
-    if n1 == 0 or n2 == 0:
-        return
-
-    print('before sum1 = %d' % sum(h1['bin_values']))
-    print('before sum2 = %d' % sum(h2['bin_values']))
-
-    if n1 > n2:
-        N = n1 / n2
-        print('N = %d' % N)
-        for idx in range(len(h1['bin_values'])):
-            h1['bin_values'][idx] /= N
-    else:
-        N = n2 / n1
-        print('N = %d' % N)
-        for idx in range(len(h2['bin_values'])):
-            h2['bin_values'][idx] /= N
-
-    print('after sum1 = %d' % sum(h1['bin_values']))
-    print('after sum2 = %d' % sum(h2['bin_values']))
 
 
 def layout() -> html.Div:
@@ -102,17 +65,14 @@ def layout() -> html.Div:
 # --------------------------------------------------------------------------------------------------
 # Collections
 
-def _collection_options(database_name: str) -> List[Dict[str, str]]:
-    collection_names = db.get_collection_names(database_name)
-    options = [{'label': name, 'value': name} for name in collection_names]
-
-    return options
-
 
 @app.callback(Output('collections-dropdown-tab2', 'options'),
               [Input('database-name-dropdown-tab2', 'value')])
-def set_lhs_collection_options(database_name: str) -> List[Dict[str, str]]:
-    return _collection_options(database_name)
+def set_collection_options(database_name: str) -> List[Dict[str, str]]:
+    """Return the list of collections for the dropdown menu."""
+    collection_names = db.get_collection_names(database_name)
+
+    return [{'label': name, 'value': name} for name in collection_names]
 
 
 # --------------------------------------------------------------------------------------------------
@@ -123,6 +83,7 @@ def set_lhs_collection_options(database_name: str) -> List[Dict[str, str]]:
               [Input('database-name-dropdown-tab2', 'value'),
                Input('collections-dropdown-tab2', 'value')])
 def update_histogram_dropdown_options(database_name: str, collection_names: List[str]) -> List[Dict[str, str]]:
+    """Return the collections' mutual list of histograms for the dropdown menu."""
     if not collection_names:
         collection_names = []
 
@@ -143,56 +104,14 @@ def update_histogram_dropdown_options(database_name: str, collection_names: List
      Input('toggle-log-tab2', 'value'),
      Input('database-name-dropdown-tab2', 'value'),
      Input('collections-dropdown-tab2', 'value')])
-def update_histogram_dropdown(histogram_name: str, log: bool, database_name: str, collection_names: List[str]) -> go.Figure:
+def update_histogram(histogram_name: str, log: bool, database_name: str, collection_names: List[str]) -> go.Figure:
+    """Plot each collection's histograms on the same plot."""
     if not collection_names:
         collection_names = []
 
     all_histograms = [db.get_histogram(histogram_name, c, database_name) for c in collection_names]
 
     return histogram_converter.n_histograms_to_plotly(all_histograms, y_log=log, no_title=True)
-
-
-# @app.callback(
-#     Output('plot-histogram-ratio-tab2', 'figure'),
-#     [Input('histogram-dropdown-tab2', 'value')],
-#     [State('database-name-dropdown-tab2', 'value'),
-#      State('collections-dropdown-tab2', 'value')])
-# def update_ratio_histogram(histogram_name: str, database_name: str, collection_names: List[str]) -> go.Figure:
-#     if not collection_names:
-#         return go.Figure()
-
-# all_histograms = [db.get_histogram(histogram_name, c, database_name) for
-# c in collection_names]
-
-#     scale_to_match(all_histograms)
-
-#     ratio_histogram = copy(lhs_histogram)
-#     for idx, bv in enumerate(rhs_histogram['bin_values']):
-#         ratio_histogram['bin_values'][idx] /= bv if bv > 0 else 1.
-
-#     return histogram_converter.histogram_to_plotly(ratio_histogram, title="Scaled to Match Ratio")
-
-
-# @app.callback(
-#     Output('plot-histogram-cdf-tab2', 'figure'),
-#     [Input('histogram-dropdown-tab2', 'value')],
-#     [State('database-name-dropdown-tab2', 'value'),
-#      State('collections-dropdown-tab2', 'value')])
-# def update_cdf_histogram(histogram_name: str, database_name: str, collection_names: List[str]) -> go.Figure:
-#     if not collection_names:
-#         return go.Figure()
-
-#     lhs_histogram = db.get_histogram(histogram_name, collection_names[0], database_name)
-#     rhs_histogram = db.get_histogram(histogram_name, collection_names[1], database_name)
-
-#     scale_to_match(all_histograms)
-
-#     ratio_histogram = cdf(lhs_histogram)
-#     for idx, bv in enumerate(cdf(rhs_histogram)['bin_values']):
-#         ratio_histogram['bin_values'][idx] /= bv if bv > 0 else 1.
-
-# return histogram_converter.histogram_to_plotly(ratio_histogram,
-# title="Scaled to Match CDF Ratio")
 
 
 # --------------------------------------------------------------------------------------------------
